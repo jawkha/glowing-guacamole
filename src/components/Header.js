da39ef75c9212c1ccde9a477a047a82f47121d16
Jawad Khawaja <devjawkha@gmail.com>
2017-11-15 15:50:40 +0100

    navigation menu toggle working but only by using jQuery :'=( not acceptable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,59 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import FaBars from "react-icons/lib/fa/bars";
 
 const Header = () => {
   return (
-    
-      <nav className="navbar fixed-top bg-light flex-column flex-md-row">
-        <NavLink className="navbar-brand" exact to="/">
-          <img
-            src="src/images/logo/hyf-logo.svg"
-            width="225"
-            height="60"
-            alt="organization logo"
-          />
+    <nav className="navbar navbar-expand-md fixed-top bg-light flex-md-row">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon">
+          <FaBars />
+        </span>
+      </button>
+      <NavLink className="navbar-brand" exact to="/">
+        <img
+          src="src/images/logo/hyf-logo.svg"
+          width="225"
+          height="60"
+          alt="organization logo"
+        />
+      </NavLink>
+      <div
+        className="collapse navbar-collapse text-center"
+        id="navbarTogglerDemo01"
+      >
+        <NavLink className="nav-item nav-link" to="/about">
+          ABOUT US
         </NavLink>
-          
-            <NavLink className="nav-item nav-link" to="/about">
-              ABOUT US
-            </NavLink>
-          
-            <NavLink className="nav-item nav-link" to="/apply">
-              APPLY NOW
-            </NavLink>
-          
-            <NavLink className="nav-item nav-link" to="/support">
-              SUPPORT US
-            </NavLink>
-          
-            <NavLink className="nav-item nav-link" to="/contact">
-              CONTACT
-            </NavLink>
-          
-        
-        <NavLink className="nav-item nav-link" to="/donate" className="btn btn-outline-danger d-none d-md-block">
-          DONATE
+
+        <NavLink className="nav-item nav-link" to="/apply">
+          APPLY NOW
         </NavLink>
-      </nav>
-    
+
+        <NavLink className="nav-item nav-link" to="/support">
+          SUPPORT US
+        </NavLink>
+
+        <NavLink className="nav-item nav-link" to="/contact">
+          CONTACT
+        </NavLink>
+      </div>
+      <NavLink
+        className="nav-item nav-link"
+        to="/donate"
+        className="btn btn-outline-danger d-none d-md-block"
+      >
+        DONATE
+      </NavLink>
+    </nav>
   );
 };
 
