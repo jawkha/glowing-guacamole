173051911dc4b9566ebf13ca35955d252aac9064
Jawad Khawaja <devjawkha@gmail.com>
2017-11-16 10:53:21 +0100

    Apply page almost complete. Bootstrap js libraries including jQuery removed.

---
+++ src/components/Apply/ApplyNowBannerImage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ApplyNowBannerImage = () => {
+    return (
+        <div className="apply-now-banner-image container-fluid text-center img-fluid py-5"></div>
+    );
+}
+
+export default ApplyNowBannerImage;
---
+++ src/components/Apply/ApplyWantToJoinUs.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ApplyWantToJoinUs = () => {
+  return (
+    <div className="apply-want-to-join-us">
+      <div className="container text-center py-5 col-md-8">
+        <h2>Want to join us?</h2>
+        <h4>
+          Application Deadline Amsterdam class 14: <br />
+          4th of December (start of class end of January) <br />
+           Application Deadline Malmö class 02: <br />
+          4th of February (start of classes ca. March 12th) <br />
+           Application Deadline Copenhagen class 05: <br />
+          4th of February (start of classes ca. March 12th)
+        </h4>
+        <p>
+          In our 6-month program we train talented refugees with and without a
+          coding background in modern JavaScript software development. Our aim
+          is to have our students land their first software development job
+          after finishing our course, and we do everything we can to prepare
+          them as well as possible. With our team of over 35 experienced senior
+          developers we have created a program around the special needs of
+          refugees. Our teachers are all volunteers, and we are all extremely
+          passionate about coding. We believe talented refugees are a great
+          opportunity for the software development world, we are here to give
+          them a helping hand.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ApplyWantToJoinUs;
